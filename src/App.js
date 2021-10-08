7574b18a8520122f1e5b8d6e0564ab3e03c38efc
kimjeyoun <wodbs5602@naver.com>
2021-10-08 16:53:43 +0900

    study_2021_10_08

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-const hi = "hi";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -14,39 +13,56 @@ const foodILike = [
     name: "ramen",
     image:
       "https://w.namu.la/s/9f15f198aab1b14c8aa47e96a91a9d03331ecb7b5b892c803159d39b0d77ab4be30e2f15f66191284d7dad8371989329cc1c80810745e980a6949ae5e3589df65640f2470b6d8c70694e0e40060b3faadcf92c770b78fdbb48baeaa524eab228",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "samgyetang",
     image:
       "https://www.chuksannews.co.kr/data/photos/20210414/art_16177684990221_d040c6.jpg",
+    rating: 4.8,
   },
   {
     id: 4,
     name: "kimbap",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/330px-Gimbap_%28pixabay%29.jpg",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "naengmyeon",
-    image:
-      "http://www.djtimes.co.kr/news/photo/202008/83537_61046_2932.jpg",
+    image: "http://www.djtimes.co.kr/news/photo/202008/83537_61046_2932.jpg",
+    rating: 4.7,
   },
 ];
 
-function Food({ name, picture }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} alt={name}/>
-    </div>;
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+Food.propTypes= {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 function App() {
   return (
     <div className="App">
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
